fc81dc669c79308b4450ced7609988a1de72012a
Sergio Gimenez <gimenezsergio123@gmail.com>
2022-08-31 09:18:11 -0300

    Empezamos a crear el objeto producto

--- producto.js
+++ producto.js
@@ -2,3 +2,22 @@ function change_image(image){
     let container = document.getElementById("main-image");
    container.src = image.src;
 }
+
+let armar_producto = function() {
+    let producto = {}
+    producto.marca = document.querySelector('.brand').innerHTML
+    producto.precio = document.querySelector('.act-price').innerHTML
+    producto.talle = document.querySelector('input[type=radio]:checked').value
+    producto.descripcion = document.querySelector('.about').innerHTML
+    producto.nombre = document.querySelector('.nombre').innerHTML
+    console.log(producto)
+
+    // Marca
+    // Precio
+    // Talle
+    // Descripcion
+    // Nombre
+    // Fotos
+}
+
+armar_producto()
