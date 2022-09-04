b06e10f141d25f75b4a9a6e9838ba743ebb5e3fc
Sergio Gimenez <gimenezsergio123@gmail.com>
2022-09-04 20:04:18 -0300

    Objeto carrito

---
+++ carrito.js
@@ -0,0 +1,15 @@
+let myCart = function cart() {
+    
+    // Objeto carrito
+    let carrito = {}
+    carrito.id = 0
+    carrito.productos = []
+    carrito.subtotal = 0
+    carrito.envio = 0
+    carrito.total = 0
+    carrito.cliente = ""
+
+    console.log(carrito)
+}
+
+myCart()
